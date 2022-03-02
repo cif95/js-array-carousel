d89ef6e827f803d3f7fe9cde4525a7b1af9fa2b3
cif95 <vivianacifarelli9@gmail.com>
2022-03-02 22:00:32 +0100

    implemented style

--- js/script.js
+++ js/script.js
@@ -29,15 +29,15 @@ let carouselThumbnailsContent = '';
 
 for (let i = 0 ; i < carouselImgs.length ; i++){
    carouselContent += 
-	`<figure class="carousel-current-item position-relative my-full-size">
-		<img class="my-full-size" src="${carouselImgs[i]}" alt="${title[i]}">
+	`<figure class="carousel-current-item position-relative">
+		<img src="${carouselImgs[i]}" alt="${title[i]}">
 		<figcaption id="carusel-info" class="position-absolute bottom-0 end-0 text-white text-end p-5">
 		<h2>${title[i]}</h2>
 		<p>${text[i]}</p>
 		</figcaption>
 	</figure>`
 	carouselThumbnailsContent += 
-	`<img class="carousel-thumbnails-item opacity-50 my-full-size" src="${carouselImgs[i]}" alt="${title[i]}">`
+	`<img class="carousel-thumbnails-item my-filter float-left" src="${carouselImgs[i]}" alt="${title[i]}">`
 }
 
 const carouselContainer = document.querySelector('div#carousel-wrapper');
@@ -57,14 +57,14 @@ const carouselElements = document.getElementsByClassName('carousel-current-item'
 carouselElements[activeItem].classList.add('active');
 
 const carouselThumbnailsElements = document.getElementsByClassName('carousel-thumbnails-item');
-carouselThumbnailsElements[activeItem].classList.remove('opacity-50');
+carouselThumbnailsElements[activeItem].classList.remove('my-filter');
 carouselThumbnailsElements[activeItem].classList.add('border', 'border-2');
 
 // ? creo lo scorrimento in avanti delle immagini al click del button next
 
 nextBtn.addEventListener ( 'click', function() {
 	carouselElements[activeItem].classList.remove('active');
-	carouselThumbnailsElements[activeItem].classList.add('opacity-50');
+	carouselThumbnailsElements[activeItem].classList.add('my-filter');
 	carouselThumbnailsElements[activeItem].classList.remove('border', 'border-2');
 	if ( activeItem === carouselImgs.length - 1 ){
 		activeItem = 0;
@@ -72,7 +72,7 @@ nextBtn.addEventListener ( 'click', function() {
 		activeItem++;
 	}
 	carouselElements[activeItem].classList.add('active');
-	carouselThumbnailsElements[activeItem].classList.remove('opacity-50');
+	carouselThumbnailsElements[activeItem].classList.remove('my-filter');
 	carouselThumbnailsElements[activeItem].classList.add('border', 'border-2');
 })
 
@@ -90,8 +90,8 @@ previousBtn.addEventListener ( 'click', function() {
 	} else {
 		activeItem--;
 	}
-	carouselThumbnailsElements[activeItem].classList.add('opacity-50');
+	carouselThumbnailsElements[activeItem].classList.add('my-filter');
 	carouselElements[activeItem].classList.add('active');
-	carouselThumbnailsElements[activeItem].classList.remove('opacity-50');
+	carouselThumbnailsElements[activeItem].classList.remove('my-filter');
 	carouselThumbnailsElements[activeItem].classList.add('border', 'border-2');
 })
